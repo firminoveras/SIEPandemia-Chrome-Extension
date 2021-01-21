048feb2c1b351f0054e396b11ca0d09fba5eb0f4
FirminoVeras <firmino.veras@live.com>
2021-01-21 14:09:23 -0300

    Melhoramentos visuais, configurações no popup

--- content.js
+++ content.js
@@ -1,72 +1,48 @@
-var enabled = false;
-
 var targetDiv = document.getElementsByClassName("btnHelpAmbiente")[0];
 
-var buttonToggleEnable = document.createElement("button");
-buttonToggleEnable.innerHTML = " SIEPandemia Desabilitado ";
-buttonToggleEnable.className = "siepandemiaButton";
-buttonToggleEnable.type = "button";
+var buttonApplyNew = document.createElement("button");
+buttonApplyNew.innerHTML = "Aplicar SIEPandemia";
+buttonApplyNew.className = "aplySiepandemia";
+buttonApplyNew.type = "button";
 
-var textAreaStudentList = document.createElement("textarea");
-textAreaStudentList.type = "text";
-textAreaStudentList.style = 'margin: 0px; width: 900px; height: 200px;';
+targetDiv.appendChild(buttonApplyNew);
 
-var buttonCopy = document.createElement("button");
-buttonCopy.innerHTML = "Copiar da Lista de Alunos";
-buttonCopy.type = "button";
-
-var buttonApply = document.createElement("button");
-buttonApply.innerHTML = "Aplicar Faltas";
-buttonApply.type = "button";
-
-buttonToggleEnable.addEventListener('click', function () {
-    enabled = !enabled;
-    if (enabled) {
-        targetDiv.appendChild(textAreaStudentList);
-        targetDiv.appendChild(buttonCopy);
-        targetDiv.appendChild(buttonApply);
-    } else {
-        targetDiv.removeChild(textAreaStudentList);
-        targetDiv.removeChild(buttonCopy);
-        targetDiv.removeChild(buttonApply);
-    }
-    buttonToggleEnable.innerHTML = " SIEPandemia " + (enabled ? "Habilitado " : "Desabilitado ");
-});
-
-buttonApply.addEventListener('click', function () {
-    var allChecks = document.getElementsByClassName('campoFrequenciaDisciplinaFJ');
-    for (var allChecksIndex = 0; allChecksIndex < allChecks.length; allChecksIndex++) {
-        if (allChecks[allChecksIndex].checked) {
-            allChecks[allChecksIndex].click();
-        }
-    };
-
-    var tableRows = document.getElementById('divListarAlunosFrequenciaDisciplina')
-        .getElementsByTagName('table')[0]
-        .getElementsByTagName('tbody')[0]
-        .getElementsByTagName('tr');
-    for (var index = 0; index < tableRows.length; index++) {
-        if (!tableRows[index].id.startsWith('trMotivo')) {
-            var nome = tableRows[index].getElementsByTagName('td')[1].getElementsByTagName('a')[0].innerHTML.trim().toUpperCase();
-            if (textAreaStudentList.value.split('\n').includes(nome)) {
-                var allChecksInRow = tableRows[index].getElementsByClassName('campoFrequenciaDisciplinaFJ');
-                for (var checkBoxIndex = 0; checkBoxIndex < allChecksInRow.length; checkBoxIndex++) {
-                    allChecksInRow[checkBoxIndex].checked = false;
-                    allChecksInRow[checkBoxIndex].click();
-                };
+buttonApplyNew.addEventListener('click', function () {
+    chrome.storage.local.get(['key'], function (result) {
+        var allChecks = document.getElementsByClassName('campoFrequenciaDisciplinaFJ');
+        for (var allChecksIndex = 0; allChecksIndex < allChecks.length; allChecksIndex++) {
+            if (allChecks[allChecksIndex].checked) {
+                allChecks[allChecksIndex].click();
+            }
+        };
+    
+        var tableRows = document.getElementById('divListarAlunosFrequenciaDisciplina')
+            .getElementsByTagName('table')[0]
+            .getElementsByTagName('tbody')[0]
+            .getElementsByTagName('tr');
+        for (var index = 0; index < tableRows.length; index++) {
+            if (!tableRows[index].id.startsWith('trMotivo')) {
+                var nome = tableRows[index].getElementsByTagName('td')[1].getElementsByTagName('a')[0].innerHTML.trim().toUpperCase();
+                if (result.key.split('\n').includes(nome)) {
+                    var allChecksInRow = tableRows[index].getElementsByClassName('campoFrequenciaDisciplinaFJ');
+                    for (var checkBoxIndex = 0; checkBoxIndex < allChecksInRow.length; checkBoxIndex++) {
+                        allChecksInRow[checkBoxIndex].checked = false;
+                        allChecksInRow[checkBoxIndex].click();
+                    };
+                }
             }
         }
-    }
-
-    var justification = document.getElementsByClassName('selectMotivoFaltaJustificada');
-    for (var index = 0; index < justification.length; index++) justification[index].selectedIndex = 9;
-
-    document.getElementById("chkConclusaoFrequenciaDiaria").checked = true;
-
-    document.getElementById("btnGravarFrequenciaDisciplina").click();
+    
+        var justification = document.getElementsByClassName('selectMotivoFaltaJustificada');
+        for (var index = 0; index < justification.length; index++) justification[index].selectedIndex = 9;
+    
+        document.getElementById("chkConclusaoFrequenciaDiaria").checked = true;
+    
+        document.getElementById("btnGravarFrequenciaDisciplina").click();
+    });
 });
 
-buttonCopy.addEventListener('click', function () {
+function getNames() {
     var tableRows = document.getElementById('divListarAlunosFrequenciaDisciplina')
         .getElementsByTagName('table')[0]
         .getElementsByTagName('tbody')[0]
@@ -74,12 +50,14 @@ buttonCopy.addEventListener('click', function () {
 
     var allNames = "";
     for (var index = 0; index < tableRows.length; index++) {
-        if (!tableRows[index].id.startsWith('trMotivo')) {
+        if (!tableRows[index].id.startsWith('trMotivo') && tableRows[index].getElementsByTagName('input')[0].checked) {
             allNames += tableRows[index].getElementsByTagName('td')[1]
                 .getElementsByTagName('a')[0].innerHTML.trim().toUpperCase() + '\n';
         }
     }
-    textAreaStudentList.value = allNames;
-});
+    return allNames;
+}
 
-targetDiv.appendChild(buttonToggleEnable);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.type == "studentList") sendResponse(getNames());
+});
---
+++ popup.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var div = document.querySelector('#divCheckArea');
+    var savedValue;
+
+    chrome.storage.local.get(['key'], function (result) {
+        document.querySelector('#textAreaStudentsName').value = result.key.trim();
+        savedValue = result.key.trim();
+    });
+
+    document.querySelector('#buttonApply').addEventListener('click', function () {
+        chrome.storage.local.set({ key: document.querySelector('#textAreaStudentsName').value.trim() }, function () {
+            document.querySelector('#buttonApply').style = 'background-color: #5E81AC';
+            savedValue = document.querySelector('#textAreaStudentsName').value.trim()
+        });
+    });
+
+    document.querySelector('#buttonAjuda').addEventListener('click', function () {
+        window.open('https://github.com/firminoveras/SIEPandemia-Chrome-Extension');
+    });
+
+    document.querySelector('#textAreaStudentsName').addEventListener('input', function () {
+        if (document.querySelector('#textAreaStudentsName').value != savedValue) {
+            document.querySelector('#buttonApply').style = 'background-color: #BF616A';
+        } else {
+            document.querySelector('#buttonApply').style = 'background-color: #5E81AC';
+        }
+    });
+
+    document.querySelector('#buttonCopiar').addEventListener('click', function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { type: "studentList" }, function (response) {
+                document.querySelector('#textAreaStudentsName').value = response.trim();
+            });
+        });
+    });
+
+});
